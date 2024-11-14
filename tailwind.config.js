53b83ea73d1c2d091ea3cd3d6fb637298657ce36
noyondev0 <mdnoyonss171@gmail.com>
2024-11-14 18:13:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        main:"url(/images/banner.png)"
+      }
+    },
+  },
+  plugins: [],
+}
+
